5af7766f17cef8c9f8927d25ee37c0f0e776c01e
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-11-16 15:27:04 +0800

    Update syllables.py

--- readability/text/syllables.py
+++ readability/text/syllables.py
@@ -13,7 +13,22 @@ def count(word):
     if len(word) <= 3:
         return 1
 
+    count = 0
+
+    if word.endswith('ly'):
+        count = 1
+        word = word[:-2]
+        
+    if word.endswith('ment') or word.endswith('ness'):
+        count = 1
+        word = word[:-4]
+
+    if word.endswith('ed'):
+        count = 0
+        word = word[:-2]
+
     word = re.sub('(?:[^laeiouy]es|[^laeiouy]e)$', '', word) # removed ed|
     word = re.sub('^y', '', word)
     matches = re.findall('[aeiouy]{1,2}', word)
-    return len(matches)
+    
+    return len(matches)+count
